a9fd294eaa95b423f286d8f8436ee55813ab83f4
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-02-24 20:00:33 -0300

    Setup OvernightJS

---
+++ test/jest.setup.ts
@@ -0,0 +1,9 @@
+// inicializa o server para todos os testes
+import { SetupServer } from "@src/server";
+import supertest from "supertest";
+
+beforeAll(() => {
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
